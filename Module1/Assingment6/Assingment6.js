cfadc792f5f4db2190d6abd1b430259ffb948b3e
Your Name <aleksi.ruotsalo@hotmail.fi>
2025-04-07 19:35:38 +0300

    Module 1 assingment 6 complete

--- Module1/Assingment6/Assingment6.js
+++ Module1/Assingment6/Assingment6.js
@@ -1,14 +1,20 @@
 'use strict';
 
-confirm()
-if user selection "ok" ->
-const num = prompt("Syötä luku:");
-
-result = '';
-
-const num = prompt("Anna vuosiluku:");
-
+const calculate = confirm("Should i calculate the square root?")
 let result = '';
+
+if (calculate) {
+    const number = Number(prompt("Enter a number"));
+
+    if (number < 0) {
+        result = "The square root of a negative number is not defined.";
+    } else {
+        const sqroot = Math.sqrt(number);
+        result = `The square root of ${number} is ${sqroot}.`;
+    }
+} else {
+    result = "The square root is not calculated.";
+}
 
 console.log(result);
 document.querySelector('#target').innerHTML = result;
