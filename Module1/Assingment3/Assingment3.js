577f2a1f28445d5c6226cb2471d6aca8ea8a4483
Your Name <aleksi.ruotsalo@hotmail.fi>
2025-04-07 17:04:20 +0300

    Module 1 assingment 3 complete

--- Module1/Assingment3/Assingment3.js
+++ Module1/Assingment3/Assingment3.js
@@ -13,6 +13,7 @@ console.log("Sum:", sum);
 console.log("Product:", product);
 console.log("Avrage:", average);
 
+document.querySelector('#target').innerHTML = "Syötetyt arvot: " + num1 + "," + num2 + "," + num3 + "<br/>";
 document.querySelector('#target').innerHTML = "Sum: " + sum + "<br/>";
 document.querySelector('#target').innerHTML += "Product: " + product + "<br/>";
 document.querySelector('#target').innerHTML += "Average: " + average + "<br/>";
