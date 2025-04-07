1ff15ba47810c8c55c940ca5f1c5f601f0189a70
Your Name <aleksi.ruotsalo@hotmail.fi>
2025-04-07 16:25:32 +0300

    Module 1 assingment 3 progress

---
+++ Module1/Assingment3/Assingment3.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const num1 = Number(prompt("Anna kokonaisluku:"));
+const num2 = Number(prompt("Anna kokonaisluku:"));
+const num3 = Number(prompt("Anna kokonaisluku:"));
+
+let sum = num1 + num2 + num3;
+let product = num1 * num2 * num3;
+let average = sum / 3;
+
+console.log("Syötyt arvot:", num1, num2, num3);
+console.log("Sum:", sum);
+console.log("Product:", product);
+console.log("Avrage:", average);
+
+document.querySelector('#target').innerHTML = result;
