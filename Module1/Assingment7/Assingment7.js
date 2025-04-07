7772eb73789773c585809f0b2c8cedbe222dfb0a
Your Name <aleksi.ruotsalo@hotmail.fi>
2025-04-07 20:10:31 +0300

    Module 1 assingment 7 progress

--- Module1/Assingment7/Assingment7.js
+++ Module1/Assingment7/Assingment7.js
@@ -1,7 +1,5 @@
 'use strict';
-
-const year = prompt("Montako kertaa noppaa heitetään?:");
-
+const rolls = prompt("Montako kertaa noppaa heitetään?:");
 let result = '';
 
 // if (year  % 4 === 0 && year % 100 !== 0 || year % 400 === 0) {
@@ -9,6 +7,25 @@ let result = '';
 // } else {
 //     result = `${year} ei ole karkausvuosi.`;
 // }
+
+// while True:
+//     arpakuutio = input("Arpakuutioden lukumäärä: ")
+//     arpakuutio = int(arpakuutio)
+//     if arpakuutio < 1 or arpakuutio > 1000:
+//         print("Virheellinen luku, syötä arvo 1 ja 1000. välillä.")
+//     else:
+//         break
+//
+// numbers = []
+//
+// import random
+// for x in range(arpakuutio): # x on loopin toimivuutta varten
+//     number = random.randint(1, 6)
+//     numbers.append(number)
+//
+// total = sum(numbers)
+// print (numbers)
+// print ("Numeroiden summa yht: " + str(total))
 
 console.log(result);
 document.querySelector('#target').innerHTML = result;
