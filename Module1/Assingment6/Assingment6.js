589267d5e1cb3d2c67c835bc9fe173c1e915ddf6
Your Name <aleksi.ruotsalo@hotmail.fi>
2025-04-07 19:25:44 +0300

    Module 1 assingment 6 progress

--- Module1/Assingment6/Assingment6.js
+++ Module1/Assingment6/Assingment6.js
@@ -1,3 +1,14 @@
 'use strict';
 
-const valinta = prompt("OK / cancel:");
+confirm()
+if user selection "ok" ->
+const num = prompt("Syötä luku:");
+
+result = '';
+
+const num = prompt("Anna vuosiluku:");
+
+let result = '';
+
+console.log(result);
+document.querySelector('#target').innerHTML = result;
