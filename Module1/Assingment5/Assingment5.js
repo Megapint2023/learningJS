324534970d427283c4a3f03a0fba426571e88f3a
Your Name <aleksi.ruotsalo@hotmail.fi>
2025-04-07 19:10:30 +0300

    Module 1 assingment 5 complete

--- Module1/Assingment5/Assingment5.js
+++ Module1/Assingment5/Assingment5.js
@@ -4,9 +4,11 @@ const year = prompt("Anna vuosiluku:");
 
 let result = '';
 
-if ((year  % 4 === 0 && year % 100 !== 0 || year % 400 === 0) {
+if (year  % 4 === 0 && year % 100 !== 0 || year % 400 === 0) {
     result = `${year} on karkausvuosi.`;
 } else {
     result = `${year} ei ole karkausvuosi.`;
 }
 
+console.log(result);
+document.querySelector('#target').innerHTML = result;
