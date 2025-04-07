a3ade5cc33d05e4ddbdf5a21959a9d3dcbc0f695
Your Name <aleksi.ruotsalo@hotmail.fi>
2025-04-07 23:46:14 +0300

    Module 1 assingment 10 progress

--- Module1/Assingment10/Assingment10.js
+++ Module1/Assingment10/Assingment10.js
@@ -12,40 +12,21 @@ let result = '';
 if (isNaN(dices) || isNaN(luckyNumber) || dices < 1 || luckyNumber < minSum || luckyNumber > maxSum) {
     result = "Virhe! Yhteissumma ei ole mahdollinen."
 } else {
-     for (let i = 0; i < throws; i++) {
-         let sum = 0;
-         for (let j = 0; i < dices; j++) {
-             sum +=Math.floor(Math.random() * 6) + 1;
-         }
-         if (sum % i === luckyNumber) {
-             hits++;
-         }
-     }
+    for (let i = 0; i < throws; i++) {
+        let sum = 0;
+        for (let j = 0; i < dices; j++) {
+            sum += Math.floor(Math.random() * 6) + 1;
+        }
+        if (sum % i === luckyNumber) {
+            hits++;
+        }
+    }
 
+    const probability = (hits / throws) * 100;
+    result = '${dices} :lla nopalla todennäköisyys osua haluttuun summalukuun: ${luckyNumber} on ${probability}.`;
+}
 
 
  console.log(result);
  document.querySelector('#target').innerHTML = result;
-
-  import random
-
- def parametriton():
-  #!!!!! tämä "eka" while loop lisäsin, jotta tahkomäärä on järkevä eli max 100 !!!!
-     while True:
-         tahkot = input("Syötä kuinka monta tahkoa (eli sivua) nopalla on: ")
-         tahkot = int(tahkot)
-         if tahkot < 1 or tahkot > 100:
-             print ("Älä keuli... Syötä järkevä arvo 1-100 välillä: ")
-         else:
-             break
-
-     while True:
-         nopanheitto = random.randint (1, tahkot )
-         if nopanheitto == tahkot:
-             print(str(nopanheitto) + " -> Osuit maksimiin!")
-             break
-         else:
-             print ("Heitit: " + str(nopanheitto) + ":n")
-
- parametriton()
 
