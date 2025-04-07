73181fec7685edc076faec83d61cd51761a8eacb
Your Name <aleksi.ruotsalo@hotmail.fi>
2025-04-08 00:00:23 +0300

    Module 1 assingment 10 complete

--- Module1/Assingment10/Assingment10.js
+++ Module1/Assingment10/Assingment10.js
@@ -14,16 +14,15 @@ if (isNaN(dices) || isNaN(luckyNumber) || dices < 1 || luckyNumber < minSum || l
 } else {
     for (let i = 0; i < throws; i++) {
         let sum = 0;
-        for (let j = 0; i < dices; j++) {
+        for (let j = 0; j < dices; j++) {
             sum += Math.floor(Math.random() * 6) + 1;
         }
-        if (sum % i === luckyNumber) {
+        if (sum === luckyNumber) {
             hits++;
         }
     }
-
     const probability = (hits / throws) * 100;
-    result = '${dices} :lla nopalla todennäköisyys osua haluttuun summalukuun: ${luckyNumber} on ${probability}.`;
+    result = `${dices}:lla nopalla todennäköisyys osua summalukuun ${luckyNumber} ... on noin ${probability.toFixed(2)}%.`;
 }
 
 
