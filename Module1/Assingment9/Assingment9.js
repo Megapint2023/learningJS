9fa3d53d8f27c4526c9bff6c0b245008921cd8ec
Your Name <aleksi.ruotsalo@hotmail.fi>
2025-04-07 21:43:46 +0300

    Module 1 assingment 9 progress

---
+++ Module1/Assingment9/Assingment9.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const number = Number(prompt("Syötä luku:"));
+
+// let sum = 0;
+//
+// if (rolls  < 1 || rolls > 1000) {
+//     document.querySelector('#target').innerHTML = "Virhe! Syötä luku väliltä 1-1000...";
+// } else {
+//     for (let i = 0; i < rolls; i++) {
+//         const roll = Math.floor(Math.random() * 6) + 1;
+//         sum += roll;
+//     }
+//
+//     const result = `Nopanheitot yht: ${rolls}. Lukujen summa yht: ${sum}`;
+//     console.log(result);
+//     document.querySelector('#target').innerHTML = result;
+// }
