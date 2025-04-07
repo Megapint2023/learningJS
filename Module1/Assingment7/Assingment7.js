8926c54db81294f8e4a27caa47a562fc7d70d789
Your Name <aleksi.ruotsalo@hotmail.fi>
2025-04-07 20:23:27 +0300

    Module 1 assingment 7 progress

--- Module1/Assingment7/Assingment7.js
+++ Module1/Assingment7/Assingment7.js
@@ -1,31 +1,30 @@
 'use strict';
+
 const rolls = prompt("Montako kertaa noppaa heitetään?:");
-let result = '';
+let sum = '';
 
-// if (year  % 4 === 0 && year % 100 !== 0 || year % 400 === 0) {
-//     result = `${year} on karkausvuosi.`;
-// } else {
-//     result = `${year} ei ole karkausvuosi.`;
-// }
+if (year  % 4 === 0 && year % 100 !== 0 || year % 400 === 0) {
+    result = `${year} on karkausvuosi.`;
+} else {
+    result = `${year} ei ole karkausvuosi.`;
+}
 
-// while True:
-//     arpakuutio = input("Arpakuutioden lukumäärä: ")
-//     arpakuutio = int(arpakuutio)
-//     if arpakuutio < 1 or arpakuutio > 1000:
-//         print("Virheellinen luku, syötä arvo 1 ja 1000. välillä.")
-//     else:
-//         break
-//
-// numbers = []
-//
-// import random
-// for x in range(arpakuutio): # x on loopin toimivuutta varten
-//     number = random.randint(1, 6)
-//     numbers.append(number)
-//
-// total = sum(numbers)
-// print (numbers)
-// print ("Numeroiden summa yht: " + str(total))
+while True:
+//heitot = input("Arpakuutioden lukumäärä: ")
+//heitot = int(heitot)
+if (heitot < 1 or heitot > 1000) {
+
+}
+print("Virheellinen luku, syötä arvo 1 ja 1000. välillä.")
+else:
+break
+numbers = []
+import random
+for heitot in range(heitot):
+number = random.randint(1, 6)
+numbers.append(number)
+total = sum(numbers)
 
 console.log(result);
 document.querySelector('#target').innerHTML = result;
+document.querySelector('#target').innerHTML += sum;
