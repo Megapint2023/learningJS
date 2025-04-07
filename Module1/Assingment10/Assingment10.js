8b24b7ce8bf2b0f4a2296c9dc1965f35039de9a4
Your Name <aleksi.ruotsalo@hotmail.fi>
2025-04-07 22:57:37 +0300

    Module 1 assingment 10 progress

--- Module1/Assingment10/Assingment10.js
+++ Module1/Assingment10/Assingment10.js
@@ -28,3 +28,25 @@ let result = '';
 
  console.log(result);
  document.querySelector('#target').innerHTML = result;
+
+//  import random
+//
+// def parametriton():
+//  #!!!!! tämä "eka" while loop lisäsin, jotta tahkomäärä on järkevä eli max 100 !!!!
+//     while True:
+//         tahkot = input("Syötä kuinka monta tahkoa (eli sivua) nopalla on: ")
+//         tahkot = int(tahkot)
+//         if tahkot < 1 or tahkot > 100:
+//             print ("Älä keuli... Syötä järkevä arvo 1-100 välillä: ")
+//         else:
+//             break
+//
+//     while True:
+//         nopanheitto = random.randint (1, tahkot )
+//         if nopanheitto == tahkot:
+//             print(str(nopanheitto) + " -> Osuit maksimiin!")
+//             break
+//         else:
+//             print ("Heitit: " + str(nopanheitto) + ":n")
+//
+// parametriton()
