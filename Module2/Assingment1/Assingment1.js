76b85400a45ac2d9084ca821d8aabc67e41e826a
Your Name <aleksi.ruotsalo@hotmail.fi>
2025-04-08 15:49:02 +0300

    Module 2 assingment 1 progress

---
+++ Module2/Assingment1/Assingment1.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const number = Number(prompt("Syötä luku: "));
+let numbers = []
+
+for (let number >= 5; i < numbers; i++) {
+        let number += 0;
+        for (let j = 0; j < numbers; j++) {
+            append numbers
+    }
+    const ;
+    result = `${number}${numbers}`;
