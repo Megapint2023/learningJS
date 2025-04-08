371c75504fc0e2e0289c3443ead0df902d32c22a
Your Name <aleksi.ruotsalo@hotmail.fi>
2025-04-08 16:01:24 +0300

    Module 2 assingment 1 complete

--- Module2/Assingment1/Assingment1.js
+++ Module2/Assingment1/Assingment1.js
@@ -2,8 +2,12 @@
 
 const numbers = [];
 
-
 for (let i = 0; i < 5; i++) {
-        const input = Number(promt(Syötä luku ${i +1}));
-        number.push(input);
+    const input = Number(prompt(`Syötä luku ${i + 1}/5:`));
+    numbers.push(input);
+}
+
+console.log("Numbers in reversed order:");
+for (let i = numbers.length - 1; i >= 0; i--) {
+    console.log(numbers[i]);
 }
