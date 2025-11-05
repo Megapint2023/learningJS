3a3a56afd563248fa43295497d11cebeeb4842ef
Your Name <aleksi.ruotsalo@hotmail.fi>
2025-11-05 15:55:24 +0200

    js testing

---
+++ TEST/test1.js
@@ -0,0 +1,6 @@
+// 'use strict';
+// console.log("Im printing to console!... whatever it even means.");
+
+'use strict';
+const name = prompt("Kuka olet?");
+document.querySelector('#otsikko').innerHTML = 'Good morning, ' + name + '!';
