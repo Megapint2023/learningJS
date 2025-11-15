e41b9d447b9fe0c928a531c4fe4b59310d510566
Your Name <aleksi.ruotsalo@hotmail.fi>
2025-11-15 16:55:54 +0200

    Module 2 assignment 4 complete

--- Module2/Assingment4/M2Assingment4.js
+++ Module2/Assingment4/M2Assingment4.js
@@ -8,15 +8,17 @@ while (true) {
     if (number === zero) break;
     numbers.push(number);
 }
+// NUMEROJÖRJESTYS (pyydetty = suurin -> pienin)
 numbers.sort((a, b) => b - a);
-
+// PRINTTI CONSOLIIN
 for (const number of numbers) {
     console.log(number)
 }
-
+//PRINTTI HTML
+let listHTML = '<ul>'; // AVAA LISTA!
 for (const number of numbers) {
-    listHTML += '<li>${number}</li>';
+    listHTML += `<li>${number}</li>`; // LUO LISTA (huomioi hipsut poikkeaa)
 }
-
-listHTML += '</ul>'
+// `` näytä tarttee silloin kun haluaa tunkee listan sisään muuttujan
+listHTML += '</ul>' //SULJE LISTA!
 document.querySelector('#target').innerHTML = listHTML;
