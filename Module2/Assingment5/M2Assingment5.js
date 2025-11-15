98da6049b78ca0f4381f22af4ef27f3c01bfe320
Your Name <aleksi.ruotsalo@hotmail.fi>
2025-11-15 19:18:35 +0200

    Module 2 assignment 5 progress

--- Module2/Assingment5/M2Assingment5.js
+++ Module2/Assingment5/M2Assingment5.js
@@ -1,12 +1,26 @@
-<!-- Write a program that prompts the user for numbers.
-When the user enters one of the numbers he previously entered,
-the program will announce that the number has already been given
-and stops its operation and then prints all the given numbers
-to the console in ascending order. (2p) -->
-
 'use strict';
 
-const muuttuja = Number(prompt("Arvo:"));
+const numbers = [];
 
-console.log(muuttuja)
+while (true) {
+    let number = Number(prompt("Syötä luku:"));
+    if (numbers.includes(number)) {
+        alert("Syötit saman luvun kahdesti!")
+        break;
+    }
+
+    numbers.push(number)
+}
+numbers.sort((a, b) => a - b);
+
+for (const number of numbers) {
+    console.log(number)
+}
+let listHTML = '<ul>'; //AVAA LISTA!
+for (const number of numbers) {
+    listHTML += `<li>${number}</li>`; //LUO LISTA || `` = -> (backkicks)
+}
+listHTML += '</ul>' //SULJE LISTA!
 document.querySelector('#target').innerHTML = listHTML;
+
+
