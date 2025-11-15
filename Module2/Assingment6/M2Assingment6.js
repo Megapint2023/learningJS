17b982f4a77c4579e0f5757b6319132f4cfc00f6
Your Name <aleksi.ruotsalo@hotmail.fi>
2025-11-15 21:45:50 +0200

    Module 2 assignment 6 complete

--- Module2/Assingment6/M2Assingment6.js
+++ Module2/Assingment6/M2Assingment6.js
@@ -1,22 +1,15 @@
-<!-- Write a function that returns a random dice roll between 1 and 6.
-The function should not have any parameters.
-Write a main program that rolls the dice until the result is 6.
-The main program should print out the result
-of each roll in an unordered list (<ul>). (2p) -->
-
 'use strict';
 
 const numbers = [];
 const dicemax = 6;
 
-function diceroll () {
-  return Math.floor(Math.random() * (dicemax)) + 1;
+function diceroll() {
+  return Math.floor(Math.random() * dicemax) + 1;
 }
 // Math.random() -> 0,111... - 0,999
 // Math.floor() -> pyöristää tasalukuun alaspäin
-
 while (true) {
-  const number = diceroll ();
+  const number = diceroll();
   if (numbers.includes(number))
     break;
   else
