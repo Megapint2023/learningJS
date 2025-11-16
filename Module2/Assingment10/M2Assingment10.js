c6e915f09ed23dda6368ebdf775922669d73f0d8
Your Name <aleksi.ruotsalo@hotmail.fi>
2025-11-16 12:06:23 +0200

    Module 2 Assignment 9 complete

---
+++ Module2/Assingment10/M2Assingment10.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const numbers = [2,5,7,9,10,11,16];
+
+function even(numbers) {
+    let evennumbers = [];
+    for (const number of numbers) {
+         if (number % 2 === 0) {
+             evennumbers.push(number);
+         }
+    }
+    return evennumbers;
+}
+console.log("All numbers:", numbers);
+console.log("Even numbers", even(numbers))
+
+const listHTML1 = numbers;
+const listHTML2 = even(numbers);
+document.querySelector('#target1').innerHTML = listHTML1;
+document.querySelector('#target2').innerHTML = listHTML2;
--- Module2/Assingment9/M2Assingment9.js
+++ Module2/Assingment9/M2Assingment9.js
@@ -4,11 +4,9 @@ const numbers = [2,5,7,9,10,11,16];
 
 function even(numbers) {
     let evennumbers = [];
-    for (const number of numbers) {
-         if (number % 2 === 0) {
+    for (const number of numbers)
+         if (number % 2 === 0)
              evennumbers.push(number);
-         }
-    }
     return evennumbers;
 }
 console.log("All numbers:", numbers);
