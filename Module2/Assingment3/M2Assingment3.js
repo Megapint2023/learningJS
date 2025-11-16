5871950d84908cf99db89db60b50bbcd7173d557
Your Name <aleksi.ruotsalo@hotmail.fi>
2025-11-16 22:43:25 +0200

    Module 2 Assignment 3 complete

--- Module2/Assingment3/M2Assingment3.js
+++ Module2/Assingment3/M2Assingment3.js
@@ -1,23 +1,14 @@
 'use strict';
 
 const dogs = 6;
-const names = [];
 
-for (let i = 0; i < dogs; i++) {
-  const name = prompt(`Koiran ${i + 1}/${dogs} nimi:`);
+const names = [];
+for (let x = 0; x < dogs; x++) {
+  const name = prompt(`Koiran ${x + 1}/${dogs} nimi:`);
   names.push(name);
 }
 
-for (let i = 0; i < names.length - 1; i++) {
-  for (let j = 0; j < names.length - i - 1; j++) {
-    if (names[j].toLowerCase() < names[j + 1].toLowerCase()) {
-      let temp = names[j];
-      names[j] = names[j + 1];
-      names[j + 1] = temp;
-    }
-  }
-}
-
+names.sort().reverse();
 let listHTML = '<ul>';
 for (const name of names) {
   listHTML += `<li>${name}</li>`;
