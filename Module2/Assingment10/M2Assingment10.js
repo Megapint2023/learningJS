8a4ec4e3e049fc5bd9950d8c9c737203a8c9cf85
Your Name <aleksi.ruotsalo@hotmail.fi>
2025-11-16 19:25:55 +0200

    Module 2 Assignment 10 complete

--- Module2/Assingment10/M2Assingment10.js
+++ Module2/Assingment10/M2Assingment10.js
@@ -11,6 +11,7 @@ for (let x = 0; x < candidates; x++) {
 const votes = Number(prompt("Äänestäjien lukumäärä: "));
 for (let vote = 0; vote < votes; vote++) {
   const newvote = prompt(`Äänestäjä ${vote + 1} kannattaa (ehdokkaan nimi):`);
+  if (newvote === '') continue;
   for (const score of names)
     if (score.name === newvote)
       score.score++;
@@ -18,7 +19,12 @@ for (let vote = 0; vote < votes; vote++) {
 
 names.sort((a, b) => b.score - a.score);
 
+console.log("Winner:", names[0]);
 console.log("Results:", names);
 
-const listHTML1 = names;
-document.querySelector('#target1').innerHTML = listHTML1;
+let listHTML = '<ul>'; //AVAA LISTA
+for (const result of names) {
+    listHTML += `<li>${result.name}: ${result.score}</li>`; //LUO LISTA || `` (backkicks)
+}
+listHTML += '</ul>'; //SULJE LISTA
+document.querySelector('#target').innerHTML = listHTML;
