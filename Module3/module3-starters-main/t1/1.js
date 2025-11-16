0997d56d328023be4696445b659999d0bcdd95ba
Your Name <aleksi.ruotsalo@hotmail.fi>
2025-11-16 19:54:03 +0200

    Module 3 Assignment 1 progress

--- Module3/module3-starters-main/t1/1.js
+++ Module3/module3-starters-main/t1/1.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const variable1 = Number(prompt(""));
+let variable2 = [];
+let variable3 = '';
+
+console.log(variable2);
+
+let listHTML = '<ul>'; //AVAA LISTA
+for (const result of names) {
+    listHTML += `<li>${variable1}</li>`; //LUO LISTA || `` (backkicks)
+}
+listHTML += '</ul>'; //SULJE LISTA
+
+
+document.querySelector('#target').innerHTML = listHTML;
