5da05151907081463bdebcee3df319456ab3f526
Your Name <aleksi.ruotsalo@hotmail.fi>
2025-11-16 22:36:10 +0200

    Module 2 Assignment 2 complete

--- Module2/Assingment2/M2Assingment2.js
+++ Module2/Assingment2/M2Assingment2.js
@@ -3,27 +3,16 @@
 const participants = Number(prompt("How many participants: "));
 
 const names = [];
-
-for (let i = 0; i < participants; i++) {
-  const name = prompt(`Participant name: ${i + 1}:`);
-  names.push(name);
+for (let x = 0; x < participants; x++) {
+  const name = prompt("Enter new participant name:")
+  names.push(name)
 }
 
-for (let i = 0; i < names.length - 1; i++) {
-  for (let j = 0; j < names.length - i - 1; j++) {
-    if (names[j].toLowerCase() > names[j + 1].toLowerCase()) {
-      let temp = names[j];
-      names[j] = names[j + 1];
-      names[j + 1] = temp;
-    }
-  }
-}
-
+names.sort();
 let listHTML = '<ol>';
 for (const name of names) {
   listHTML += `<li>${name}</li>`;
 }
-
 listHTML += '</ol>';
 
 document.querySelector('#target').innerHTML = listHTML;
