7f687187cb0e80ac4c311c9dd97549534316aa6f
Your Name <aleksi.ruotsalo@hotmail.fi>
2025-11-16 02:57:30 +0200

    Module 2 Assignment 9 complete

--- Module2/Assingment9/M2Assingment9.js
+++ Module2/Assingment9/M2Assingment9.js
@@ -4,12 +4,17 @@ const numbers = [2,5,7,9,10,11,16];
 
 function even(numbers) {
     let evennumbers = [];
-    for (const number of numbers){
-      if number is % 2 == 0;
-        evennumbers.push(number);
+    for (const number of numbers) {
+         if (number % 2 === 0) {
+             evennumbers.push(number);
+         }
     }
     return evennumbers;
 }
+console.log("All numbers:", numbers);
+console.log("Even numbers", even(numbers))
 
-const listHTML = even(numbers);
-document.querySelector('#target').innerHTML = listHTML;
+const listHTML1 = numbers;
+const listHTML2 = even(numbers);
+document.querySelector('#target1').innerHTML = listHTML1;
+document.querySelector('#target2').innerHTML = listHTML2;
