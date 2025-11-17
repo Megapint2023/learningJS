d070dbfbc44f7dc6c98604bfe4319e46beceb11a
Your Name <aleksi.ruotsalo@hotmail.fi>
2025-11-17 15:41:35 +0200

    Module 3 assingment 2 progress

--- Module3/module3-starters-main/t2/2.js
+++ Module3/module3-starters-main/t2/2.js
@@ -2,12 +2,23 @@
 
 const target = document.getElementById('target')
 
-const listHTML =
-    `
-    <li>First Item</li>
-    <li>Second Item</li>
-    <li>Third Item</li>
-    `;
+const l1 = document.createElement('li')
+l1.textContent = 'First item';
+target.appendChild(l1);
 
-target.innerHTML = listHTML;
-target.classList.add('my-list');
+const l2 = document.createElement('li')
+l2.classList.add('my-list');
+l2.textContent = 'First item';
+target.appendChild(l2);
+
+const l3 = document.createElement('li')
+l3.textContent = 'First item';
+target.appendChild(l3);
+
+/*  Open t2 folder in your IDE/editor. <br>
+    Add HTML by using createElement() and appendChild methods. <br>
+    Add the following HTML code to the element with id="target" <br>
+&lt;li&gt;First item&lt;/li&gt; <br>
+&lt;li&gt;Second item&lt;/li&gt; <br>
+&lt;li&gt;Third item&lt;/li&gt; <br>
+Add class my-item to the second list item. */
