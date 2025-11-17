5245824c1fa25d30b503f85111bb8ac157cc0ebf
Your Name <aleksi.ruotsalo@hotmail.fi>
2025-11-17 16:10:20 +0200

    Module 3 assingment 3 complete

--- Module3/module3-starters-main/t3/3.js
+++ Module3/module3-starters-main/t3/3.js
@@ -1,26 +1,9 @@
 'use strict';
-const target = document.getElementById('target')
 
 const names = ['John', 'Paul', 'Jones'];
 
-const list = [];
-for (let x = 0; x < names; x++) {
-  const item = list;
-  list.push(item)
+let listHTML = '';
+for (let x = 0; x < names.length; x++) {
+  listHTML += `<li>${names[x]}</li>`;
 }
-
-let list =  listHTML;
-for (const result of names) {
-    listHTML += `<li>${item.list}</li>`;
-}
-
-target.appendChild(l1);
-document.querySelector('#target').innerHTML = listHTML;
-
-/* Open t3 folder in your IDE/editor. <br>
-    Add HTML by using innerHTML property. <br>
-    Add the following HTML code to the element with id="target". <br>
-    Add the values from 'names' array to the &lt;li&gt; elements in a for-loop. <br>
-&lt;li&gt;John&lt;/li&gt; <br>
-&lt;li&gt;Paul&lt;/li&gt; <br>
-&lt;li&gt;Jones&lt;/li&gt; */
+document.getElementById('target').innerHTML = listHTML;
